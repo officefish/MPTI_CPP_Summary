48da985ad53cda223e3cd08e124c48c0304fa7ff
inozemcev (windows) <= officefish>
2021-02-21 21:14:17 +0300

    Добавьте файлы проекта.

---
+++ MPTI_CPP_Summary/MPTI_CPP_Summary.cpp
@@ -0,0 +1,33 @@
+﻿// MPTI_CPP_Summary.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+
+
+/*
+Курс читает Мищерин Илья.
+
+Книги для чтения:
+
+"Язык программирования C++". Бьёрн Страуструп
+"Effective C++", "More effective C++", "Effective modern C++". Скотт Дуглас Майерс
+
+С++ Standard / pdf /
+
+https://en.cppreference.com/w/
+
+*/
+
+#include "Summary.h"
+
+
+#include <bits/stdc++.h>
+
+using std::cout;
+
+
+int main()
+{
+    
+    cout << "MPTI C++ Summary \n";
+
+    return Summary0101(); // History, Common Words;
+}
+
---
+++ MPTI_CPP_Summary/Summary0101.cpp
@@ -0,0 +1,31 @@
+#include "Summary.h"
+
+int Summary0101() {
+
+	/*
+	* Bjarne Stroutsrup (Bell Labs), 80s
+	* C language program translator
+	*
+	* basic elements for compilator programs
+	* 1998 / 2003 / 2005 Technical report 1
+	*
+	* C++11 / C++14 / C++17 / C++20 / ... every three years next
+	*
+	* Phylosophy:
+	* -  backward compatible with C
+	* -  Multy stype: OOP / Procedure programming
+	* -  Free, don't "pay: for what you don't use
+	* -  Platform independent
+	*
+	* Why C++?! /++/ - is increment
+	*
+	* C++ lets work with memory what makes it both powerfull and difficult to work
+	*
+	*
+	*/
+
+	return 0;
+
+}
+
+
